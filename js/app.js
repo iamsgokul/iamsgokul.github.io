81b9e4284c5247c9f17e1f3cb80562233d506d92
iamsgokul <iamsgokul@gmail.com>
2020-11-24 17:41:37 +0530

    Search Design Update

--- js/app.js
+++ js/app.js
@@ -3,9 +3,3 @@ $(function () {
     $('#footerbar').load('footer.html');
     $('#watchread').load('watchread.html');
 })
-
-$(document).delegate(function(){
-    $('i.gwicon-menu').on('click', function () {
-        $('header nav ul').toggleClass('open');
-    })
-})
