fbf86d4985eb6ee63bc9863bd0979e4d603a6e05
iamsgokul <iamsgokul@gmail.com>
2022-04-01 18:58:07 +0530

    Stock Average Price Calculator

--- js/jas.js
+++ js/jas.js
@@ -1,8 +1,8 @@
 /*- Responsive Check Start -*/
 function loadUrl() {
-  var elems = document.getElementsByClassName('device-cont');
+  var elems = document.getElementsByClassName("device-cont");
   for (var i = 0; i < elems.length; i += 1) {
-    elems[i].style.display = 'block';
+    elems[i].style.display = "block";
   }
   document.getElementById("resp1").src = document.getElementById("url").value;
   document.getElementById("resp2").src = document.getElementById("url").value;
@@ -20,27 +20,44 @@ function loadUrl() {
 /*- Responsive Check End -*/
 /*-Character Count Start-*/
 function countit(what) {
-  formcontent = what.form.charcount.value
-  what.form.displaycount.value = formcontent.length
+  formcontent = what.form.charcount.value;
+  what.form.displaycount.value = formcontent.length;
 }
 /*-Character Count End-*/
 
 /*-CSS Mini Start-*/
 function get(a) {
-  return document.getElementById(a)
+  return document.getElementById(a);
 }
 
 function highlightCode(a) {
   if (hc.checked) {
     var b = a.innerHTML;
-    b = b.replace(/\{([\s\S]+?)\}/g, function (a) {
-      return a.replace(/\'(.*?)\'/g, "<span class='st'>'$1'</span>").replace(/\"(.*?)\"/g, "<span class='st'>\"$1\"</span>").replace(/(\{|\n|;)?(.[^\{]*?):(.[^\{]*?)(;|\})/g, "$1<span class='pr'>$2</span>:<span class='vl'>$3</span>$4").replace(/<span class='pr'>\{/g, "{<span class='pr'>")
-    }), b = b.replace(/&lt;(.*?)('|")(.*?)('|")&gt;/g, function (a) {
-      return a.replace(/'(.*?)'/g, "<span class='vl'>'$1'</span>").replace(/"(.*?)"/g, "<span class='vl'>\"$1\"</span>")
-    }), b = b.replace(/\{([\s\S]+?)\}/g, function (a) {
-      return a.replace(/([\(\)\{\}\[\]\:\;\,]+)/g, "<span class='pn'>$1</span>").replace(/\!important/gi, "<span class='im'>!important</span>")
-    }), b = b.replace(/\/\*([\w\W]+?)\*\//gm, "<span class='cm'>/*$1*/</span>"), a.innerHTML = "<code>" + b + "</code>", hr.style.display = "block", rt.style.display = "block"
-  } else hr.style.display = "none", rt.style.display = "none"
+    (b = b.replace(/\{([\s\S]+?)\}/g, function (a) {
+      return a
+        .replace(/\'(.*?)\'/g, "<span class='st'>'$1'</span>")
+        .replace(/\"(.*?)\"/g, "<span class='st'>\"$1\"</span>")
+        .replace(
+          /(\{|\n|;)?(.[^\{]*?):(.[^\{]*?)(;|\})/g,
+          "$1<span class='pr'>$2</span>:<span class='vl'>$3</span>$4"
+        )
+        .replace(/<span class='pr'>\{/g, "{<span class='pr'>");
+    })),
+      (b = b.replace(/&lt;(.*?)('|")(.*?)('|")&gt;/g, function (a) {
+        return a
+          .replace(/'(.*?)'/g, "<span class='vl'>'$1'</span>")
+          .replace(/"(.*?)"/g, "<span class='vl'>\"$1\"</span>");
+      })),
+      (b = b.replace(/\{([\s\S]+?)\}/g, function (a) {
+        return a
+          .replace(/([\(\)\{\}\[\]\:\;\,]+)/g, "<span class='pn'>$1</span>")
+          .replace(/\!important/gi, "<span class='im'>!important</span>");
+      })),
+      (b = b.replace(/\/\*([\w\W]+?)\*\//gm, "<span class='cm'>/*$1*/</span>")),
+      (a.innerHTML = "<code>" + b + "</code>"),
+      (hr.style.display = "block"),
+      (rt.style.display = "block");
+  } else (hr.style.display = "none"), (rt.style.display = "none");
 }
 
 function compressCSS(a) {
@@ -48,20 +65,49 @@ function compressCSS(a) {
     c = /@(media|-w|-m|-o|keyframes|page)(.*?)\{([\s\S]+?)?\}\}/gi,
     d = b.value,
     e = d.length;
-  d = rc.checked || sc.checked ? d.replace(/\/\*[\w\W]*?\*\//gm, "") : d.replace(/(\n+)?(\/\*[\w\W]*?\*\/)(\n+)?/gm, "\n$2\n"), d = d.replace(/([\n\r\t\s ]+)?([\,\:\;\{\}]+?)([\n\r\t\s ]+)?/g, "$2"), d = sc.checked ? d : d.replace(/\}(?!\})/g, "}\n"), d = bi.checked ? d.replace(c, function (a) {
-    return a.replace(/\n+/g, "\n  ")
-  }) : d.replace(c, function (a) {
-    return a.replace(/\n+/g, "")
-  }), d = bi.checked && !sc.checked ? d.replace(/\}\}/g, "}\n}") : d, d = bi.checked && !sc.checked ? d.replace(/@(media|-w|-m|-o|keyframes)(.*?)\{/g, "@$1$2{\n  ") : d, d = cm.checked ? d.replace(/;\}/g, "}") : d.replace(/\}/g, ";}").replace(/;+\}/g, ";}").replace(/\};\}/g, "}}"), d = d.replace(/\:0(px|em|pt)/gi, ":0"), d = d.replace(/ 0(px|em|pt)/gi, " 0"), d = d.replace(/\s+\!important/gi, "!important"), d = d.replace(/(^\n+|\n+$)/, ""), b.value = d, hr.innerHTML = "/* " + (e - d.length) + " of " + e + " unused characters has been removed. */\n" + d.replace(/</g, "&lt;").replace(/>/g, "&gt;"), highlightCode(hr)
+  (d =
+    rc.checked || sc.checked
+      ? d.replace(/\/\*[\w\W]*?\*\//gm, "")
+      : d.replace(/(\n+)?(\/\*[\w\W]*?\*\/)(\n+)?/gm, "\n$2\n")),
+    (d = d.replace(/([\n\r\t\s ]+)?([\,\:\;\{\}]+?)([\n\r\t\s ]+)?/g, "$2")),
+    (d = sc.checked ? d : d.replace(/\}(?!\})/g, "}\n")),
+    (d = bi.checked
+      ? d.replace(c, function (a) {
+          return a.replace(/\n+/g, "\n  ");
+        })
+      : d.replace(c, function (a) {
+          return a.replace(/\n+/g, "");
+        })),
+    (d = bi.checked && !sc.checked ? d.replace(/\}\}/g, "}\n}") : d),
+    (d =
+      bi.checked && !sc.checked
+        ? d.replace(/@(media|-w|-m|-o|keyframes)(.*?)\{/g, "@$1$2{\n  ")
+        : d),
+    (d = cm.checked
+      ? d.replace(/;\}/g, "}")
+      : d.replace(/\}/g, ";}").replace(/;+\}/g, ";}").replace(/\};\}/g, "}}")),
+    (d = d.replace(/\:0(px|em|pt)/gi, ":0")),
+    (d = d.replace(/ 0(px|em|pt)/gi, " 0")),
+    (d = d.replace(/\s+\!important/gi, "!important")),
+    (d = d.replace(/(^\n+|\n+$)/, "")),
+    (b.value = d),
+    (hr.innerHTML =
+      "/* " +
+      (e - d.length) +
+      " of " +
+      e +
+      " unused characters has been removed. */\n" +
+      d.replace(/</g, "&lt;").replace(/>/g, "&gt;")),
+    highlightCode(hr);
 }
 
 function clearField(a) {
   var b = get(a);
-  b.value = "", b.focus()
+  (b.value = ""), b.focus();
 }
 
 function selectAll(a) {
-  get(a).focus(), get(a).select()
+  get(a).focus(), get(a).select();
 }
 var hc = get("highlightCode"),
   rc = get("removeComments"),
@@ -81,50 +127,54 @@ var hc = get("highlightCode"),
 /*-CSS Mini End-*/
 /*- Fav Icon Start -*/
 function checkfavi(visitor_name, exten, displayObj) {
-  displayObj.value = '<link href="' + visitor_name.value + '" rel="shortcut icon" type="' + exten.value + '">';
+  displayObj.value =
+    '<link href="' +
+    visitor_name.value +
+    '" rel="shortcut icon" type="' +
+    exten.value +
+    '">';
 }
 /*- Fav Icon End -*/
 
 /*-Google Ads Start-*/
 function adsensconvert() {
-  var re = /[<>"'&]/g
+  var re = /[<>"'&]/g;
   for (i = 0; i < arguments.length; i++)
     arguments[i].value = arguments[i].value.replace(re, function (m) {
-      return removechar(m)
-    })
+      return removechar(m);
+    });
 }
 
 function removechar(match) {
-  if (match == "<")
-    return "&lt;"
-  else if (match == ">")
-    return "&gt;"
-  else if (match == "\"")
-    return "&quot;"
-  else if (match == "'")
-    return "&#039;"
-  else if (match == "&")
-    return "&amp;"
+  if (match == "<") return "&lt;";
+  else if (match == ">") return "&gt;";
+  else if (match == '"') return "&quot;";
+  else if (match == "'") return "&#039;";
+  else if (match == "&") return "&amp;";
 }
 /*-Google Ads End-*/
 
 /*- Google Direct Download Link Start-*/
 function clearFiled() {
-  document.getElementById("googledrid").style.display = "none"
+  document.getElementById("googledrid").style.display = "none";
 }
 
 function checkName(a, b) {
-  25 > a.value.length ? (alert("Please enter correct Google Drive File ID"), b.style.display = "none") : (b.value = "https://docs.google.com/uc?id=" + a.value + "&export=download", b.style.display = "block")
+  25 > a.value.length
+    ? (alert("Please enter correct Google Drive File ID"),
+      (b.style.display = "none"))
+    : ((b.value =
+        "https://docs.google.com/uc?id=" + a.value + "&export=download"),
+      (b.style.display = "block"));
 }
 /*- Google Direct Download Link End-*/
 
 /*- Meta Tag Start-*/
 function MakeIt(form) {
-  var
-    txt = '<meta name="Description" content="' + form.description.value + '">\r\n';
+  var txt =
+    '<meta name="Description" content="' + form.description.value + '">\r\n';
   txt += '<meta name="Keywords" content="';
-  if (form.keywords.value)
-    txt += form.keywords.value + '">\r\n';
+  if (form.keywords.value) txt += form.keywords.value + '">\r\n';
   txt += '<meta name="Author" content="' + form.author.value + '">\r\n';
   txt += '<meta name="Language" content="' + form.language.value + '">\r\n';
   form.source.value = txt;
@@ -137,26 +187,60 @@ function AddText(form, Action) {
 }
 /*- Meta Tag End -*/
 
-
 /*- Blogger Site Map Start-*/
 function blogSiteMap(form) {
-
-  var txt = '' + form.siteurl.value + '/sitemap.xml\r\n';
+  var txt = "" + form.siteurl.value + "/sitemap.xml\r\n";
   form.source.value = txt;
-  form.source.display = 'block';
+  form.source.display = "block";
 }
 /*- Blogger Site Map End -*/
 
 /*- Social Media Meta Tag Start-*/
-function genrate(facebookProfile, facebookPage, googleProfile, googlePage, twitterAcc, exte) {
-  exte.value = "<meta content='" + facebookProfile.value + "' property='article:author'/>\r\n" + "<meta content='" + facebookPage.value + "' property='article:publisher'/>\r\n" + "<link href='" + googleProfile.value + "' rel='article:author'/>\r\n" + "<link href='" + googlePage.value + "' rel='article:author'/>\r\n" + "<meta content='" + twitterAcc.value + "' rel='article:author'/>\r\n";
+function genrate(
+  facebookProfile,
+  facebookPage,
+  googleProfile,
+  googlePage,
+  twitterAcc,
+  exte
+) {
+  exte.value =
+    "<meta content='" +
+    facebookProfile.value +
+    "' property='article:author'/>\r\n" +
+    "<meta content='" +
+    facebookPage.value +
+    "' property='article:publisher'/>\r\n" +
+    "<link href='" +
+    googleProfile.value +
+    "' rel='article:author'/>\r\n" +
+    "<link href='" +
+    googlePage.value +
+    "' rel='article:author'/>\r\n" +
+    "<meta content='" +
+    twitterAcc.value +
+    "' rel='article:author'/>\r\n";
 }
 /*- Social Media Meta Tag End-*/
 
-
 /*- Social Media Meta Tag Start-*/
 function rest() {
-  document.getElementById("thumblin").style.display = "none", document.getElementById("thumImg1").style.display = "none", document.getElementById("thumImg2").style.display = "none", document.getElementById("thumImg3").style.display = "none", document.getElementById("thumImg4").style.display = "none", document.getElementById("thumImg5").style.display = "none", document.getElementById("thumImg6").style.display = "none", document.getElementById("thumImg7").style.display = "none", document.getElementById("downImg1").style.display = "none", document.getElementById("downImg2").style.display = "none", document.getElementById("downImg3").style.display = "none", document.getElementById("downImg4").style.display = "none", document.getElementById("downImg5").style.display = "none", document.getElementById("downImg6").style.display = "none", document.getElementById("downImg7").style.display = "none", document.getElementById("downImg8").style.display = "none"
+  (document.getElementById("thumblin").style.display = "none"),
+    (document.getElementById("thumImg1").style.display = "none"),
+    (document.getElementById("thumImg2").style.display = "none"),
+    (document.getElementById("thumImg3").style.display = "none"),
+    (document.getElementById("thumImg4").style.display = "none"),
+    (document.getElementById("thumImg5").style.display = "none"),
+    (document.getElementById("thumImg6").style.display = "none"),
+    (document.getElementById("thumImg7").style.display = "none"),
+    (document.getElementById("downImg1").style.display = "none"),
+    (document.getElementById("downImg2").style.display = "none"),
+    (document.getElementById("downImg3").style.display = "none"),
+    (document.getElementById("downImg4").style.display = "none"),
+    (document.getElementById("downImg5").style.display = "none"),
+    (document.getElementById("downImg6").style.display = "none"),
+    (document.getElementById("downImg7").style.display = "none"),
+    (document.getElementById("downImg8").style.display = "none");
 }
 
 function getThumb(e) {
@@ -164,8 +248,91 @@ function getThumb(e) {
   if (!0 === t.includes("www.youtube.com/"))
     if (t.length > 40 && t.length <= 50) {
       var m = e.value.replace(/\s+/, "").split("=");
-      m = m[m.length - 1], document.getElementById("thumblin").innerHTML = "http://img.youtube.com/vi/" + m + "/0.jpg", thumblin.length > 5 ? document.getElementById("thumblin").style.display = "none" : document.getElementById("thumblin").style.display = "block", document.getElementById("thumImg1").src = "http://img.youtube.com/vi/" + m + "/0.jpg", document.getElementById("thumImg2").src = "http://img.youtube.com/vi/" + m + "/1.jpg", document.getElementById("thumImg3").src = "http://img.youtube.com/vi/" + m + "/2.jpg", document.getElementById("thumImg4").src = "http://img.youtube.com/vi/" + m + "/3.jpg", document.getElementById("thumImg5").src = "http://img.youtube.com/vi/" + m + "/hqdefault.jpg", document.getElementById("thumImg6").src = "http://img.youtube.com/vi/" + m + "/mqdefault.jpg", document.getElementById("thumImg7").src = "http://img.youtube.com/vi/" + m + "/maxresdefault.jpg", document.getElementById("downImg1").href = "http://img.youtube.com/vi/" + m + "/0.jpg", document.getElementById("downImg2").href = "http://img.youtube.com/vi/" + m + "/1.jpg", document.getElementById("downImg3").href = "http://img.youtube.com/vi/" + m + "/2.jpg", document.getElementById("downImg4").href = "http://img.youtube.com/vi/" + m + "/3.jpg", document.getElementById("downImg5").href = "http://img.youtube.com/vi/" + m + "/hqdefault.jpg", document.getElementById("downImg6").href = "http://img.youtube.com/vi/" + m + "/mqdefault.jpg", document.getElementById("downImg7").href = "http://img.youtube.com/vi/" + m + "/maxresdefault.jpg", document.getElementById("thumblin").style.display = "block", document.getElementById("thumImg1").style.display = "block", document.getElementById("thumImg2").style.display = "block", document.getElementById("thumImg3").style.display = "block", document.getElementById("thumImg4").style.display = "block", document.getElementById("thumImg5").style.display = "block", document.getElementById("thumImg6").style.display = "block", document.getElementById("thumImg7").style.display = "block", document.getElementById("downImg1").style.display = "block", document.getElementById("downImg2").style.display = "block", document.getElementById("downImg3").style.display = "block", document.getElementById("downImg4").style.display = "block", document.getElementById("downImg5").style.display = "block", document.getElementById("downImg6").style.display = "block", document.getElementById("downImg7").style.display = "block", console.log(m)
+      (m = m[m.length - 1]),
+        (document.getElementById("thumblin").innerHTML =
+          "http://img.youtube.com/vi/" + m + "/0.jpg"),
+        thumblin.length > 5
+          ? (document.getElementById("thumblin").style.display = "none")
+          : (document.getElementById("thumblin").style.display = "block"),
+        (document.getElementById("thumImg1").src =
+          "http://img.youtube.com/vi/" + m + "/0.jpg"),
+        (document.getElementById("thumImg2").src =
+          "http://img.youtube.com/vi/" + m + "/1.jpg"),
+        (document.getElementById("thumImg3").src =
+          "http://img.youtube.com/vi/" + m + "/2.jpg"),
+        (document.getElementById("thumImg4").src =
+          "http://img.youtube.com/vi/" + m + "/3.jpg"),
+        (document.getElementById("thumImg5").src =
+          "http://img.youtube.com/vi/" + m + "/hqdefault.jpg"),
+        (document.getElementById("thumImg6").src =
+          "http://img.youtube.com/vi/" + m + "/mqdefault.jpg"),
+        (document.getElementById("thumImg7").src =
+          "http://img.youtube.com/vi/" + m + "/maxresdefault.jpg"),
+        (document.getElementById("downImg1").href =
+          "http://img.youtube.com/vi/" + m + "/0.jpg"),
+        (document.getElementById("downImg2").href =
+          "http://img.youtube.com/vi/" + m + "/1.jpg"),
+        (document.getElementById("downImg3").href =
+          "http://img.youtube.com/vi/" + m + "/2.jpg"),
+        (document.getElementById("downImg4").href =
+          "http://img.youtube.com/vi/" + m + "/3.jpg"),
+        (document.getElementById("downImg5").href =
+          "http://img.youtube.com/vi/" + m + "/hqdefault.jpg"),
+        (document.getElementById("downImg6").href =
+          "http://img.youtube.com/vi/" + m + "/mqdefault.jpg"),
+        (document.getElementById("downImg7").href =
+          "http://img.youtube.com/vi/" + m + "/maxresdefault.jpg"),
+        (document.getElementById("thumblin").style.display = "block"),
+        (document.getElementById("thumImg1").style.display = "block"),
+        (document.getElementById("thumImg2").style.display = "block"),
+        (document.getElementById("thumImg3").style.display = "block"),
+        (document.getElementById("thumImg4").style.display = "block"),
+        (document.getElementById("thumImg5").style.display = "block"),
+        (document.getElementById("thumImg6").style.display = "block"),
+        (document.getElementById("thumImg7").style.display = "block"),
+        (document.getElementById("downImg1").style.display = "block"),
+        (document.getElementById("downImg2").style.display = "block"),
+        (document.getElementById("downImg3").style.display = "block"),
+        (document.getElementById("downImg4").style.display = "block"),
+        (document.getElementById("downImg5").style.display = "block"),
+        (document.getElementById("downImg6").style.display = "block"),
+        (document.getElementById("downImg7").style.display = "block"),
+        console.log(m);
     } else alert("Please enter the valied YouTube URL");
-  else alert("Please enter the valied YouTube URL")
+  else alert("Please enter the valied YouTube URL");
 }
 /*- Social Media Meta Tag End-*/
+
+/*- Stock Avg Price Start-*/
+function calculator() {
+  var opnbtn = document.getElementsByClassName("calout")[0];
+  opnbtn.classList.add("open");
+  let shval = document.getElementById("cquantity").value;
+  let shpri = document.getElementById("cprice").value;
+  let nshval = document.getElementById("nquantity").value;
+  let nshpri = document.getElementById("nprice").value;
+
+  let oldInv = parseFloat(shval) * parseFloat(shpri);
+  document.getElementById("oldinv").innerHTML = oldInv;
+  let newInv = parseFloat(nshval) * parseFloat(nshpri);
+  document.getElementById("newinv").innerHTML = newInv;
+
+  let avgPri =
+    (parseFloat(shval) * parseFloat(shpri) +
+      parseFloat(nshval) * parseFloat(nshpri)) /
+    (parseFloat(shval) + parseFloat(nshval));
+  document.getElementById("avgPric").innerHTML = avgPri;
+
+  let totPric =
+    parseFloat(shval) * parseFloat(shpri) +
+    parseFloat(nshval) * parseFloat(nshpri);
+  document.getElementById("totalAmt").innerHTML = totPric;
+
+  let totQat = parseInt(shval) + parseInt(nshval);
+  document.getElementById("totalQat").innerHTML = totQat;
+}
+function clearFld() {
+  var clsbtn = document.getElementsByClassName("calout open")[0];
+  clsbtn.classList.remove("open");
+}
+/*- Stock Avg Price End-*/
