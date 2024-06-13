ac984093185bf6f369dd9ea0c37a258ce7a2744a
gokulakrishnan.s <gokulakrishnan.s@zohocorp.com>
2024-06-13 18:10:55 +0530

    Update QR Code

--- js/qr.js
+++ js/qr.js
@@ -13,6 +13,7 @@ function generateQRCode() {
     width: 300,
     height: 300,
   });
+  document.getElementById("qrcodeout").style.display = "block";
 }
 
 function clearQRCode() {
@@ -21,4 +22,23 @@ function clearQRCode() {
 
   var qrcodeContainer = document.getElementById("qrcode");
   qrcodeContainer.innerHTML = ""; // Clear previous QR Code
+  document.getElementById("qrcodeout").style.display = "none";
+}
+
+function downloadQRCode() {
+  var qrcodeContainer = document
+    .getElementById("qrcode")
+    .getElementsByTagName("img")[0];
+  if (!qrcodeContainer) {
+    alert("No QR code to download!");
+    return;
+  }
+
+  var imgSrc = qrcodeContainer.src;
+  var link = document.createElement("a");
+  link.href = imgSrc;
+  link.download = "qrcode.png";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
