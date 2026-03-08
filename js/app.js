21d0810a6cc372bb401fe7628375171efd4cd36d
Gokul S <22526771+iamsgokul@users.noreply.github.com>
2026-03-08 11:26:19 +0530

    Refactor DOMContentLoaded event handling and initNav

--- js/app.js
+++ js/app.js
@@ -4,44 +4,43 @@
 //     $('#watchread').load('watchread.html');
 // })
 
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("nav.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+      initNav();
+    });
 
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('nav.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        });
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footerbar").innerHTML = data;
+    });
 
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footerbar').innerHTML = data;
-        });
-
-    fetch('watchread.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('watchread').innerHTML = data;
-        });
+  fetch("watchread.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("watchread").innerHTML = data;
+    });
 });
 
-  const menuopen = document.querySelector('header nav > a');
-  menuopen.addEventListener('click', function (e) {
+function initNav() {
+  const menuopen = document.querySelector("header nav > a");
+  menuopen.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector('.topmenu').classList.toggle('open');
-  })
+    document.querySelector(".topmenu").classList.toggle("open");
+  });
 
-  function setActiveNavLink() {
-    const currentUrl = window.location.href;
-    const navLinks = document.querySelectorAll('.topmenu a');
+  const currentUrl = window.location.href;
+  const navLinks = document.querySelectorAll(".topmenu a");
 
-    navLinks.forEach(link => {
-      if (link.href === currentUrl) {
-        link.parentElement.classList.add('active');
-      } else {
-        link.parentElement.classList.remove('active');
-      }
-    });
-  }
+  navLinks.forEach((link) => {
+    if (link.href === currentUrl) {
+      link.parentElement.classList.add("active");
+    } else {
+      link.parentElement.classList.remove("active");
+    }
+  });
+}
 
-  setActiveNavLink();
